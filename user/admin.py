ab8705e0dd48cd761f573cecf5f7ebf75ad3a998
Sharanya Bammidi <126247117+sisira1485@users.noreply.github.com>
2024-03-27 22:15:51 +0530

    ...

--- user/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import UserRegistrationModel, UserTextDataModel
-
-# Register your models here.
-admin.site.register(UserRegistrationModel)
-admin.site.register(UserTextDataModel)
